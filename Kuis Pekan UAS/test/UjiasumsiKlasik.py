bac9e60a551a1faf9353f149e6f4575b7f43d21f
DimasBrahmantya <127276883+DimasBrahmantya@users.noreply.github.com>
2024-01-11 09:49:17 +0700

    Add files via upload

---
+++ Kuis Pekan UAS/test/UjiasumsiKlasik.py
@@ -0,0 +1,9 @@
+# Daftar variabel yang ingin dianalisis
+variable_combinations = [
+    ('age', ['intubed']),
+    ('pneumonia', ['sex', 'pregnancy']),
+    ('diabetes', ['asthma']),
+    ('inmsupr', ['hypertension', 'cardiovascular']),
+    ('obesity', ['sex']),
+    # Tambahkan kombinasi variabel lainnya sesuai kebutuhan
+]
